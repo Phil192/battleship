d8f737afa185b7010aa5ef3ea2225cd3370fbdb2
ubik <phil192@yandex.ru>
2017-05-13 00:07:57 +0300

    go version added

--- battleship1.go
+++ battleship1.go
@@ -2,105 +2,102 @@ package main
 
 import (
 	"fmt"
-  "math/rand"
-  "strconv"
-  "time"
-  )
-
+	"math/rand"
+	"strconv"
+	"time"
+)
 
 var board [8][8]string
 
 func random_number(num int) int {
-  seed := rand.NewSource(time.Now().UnixNano())
-  random := rand.New(seed)
-  random_number := random.Intn(num)
-  return random_number
+	seed := rand.NewSource(time.Now().UnixNano())
+	random := rand.New(seed)
+	random_number := random.Intn(num)
+	return random_number
 }
 
 func painting_sea() {
-  for i := 0; i < 8; i++ {
-    for j := 0; j < 8; j++ {
-      board[i][j] = "~"
-    }
-  }
+	for i := 0; i < 8; i++ {
+		for j := 0; j < 8; j++ {
+			board[i][j] = "~"
+		}
+	}
 }
 
 func random_direction() string {
-  random_number := random_number(2)
-  if random_number == 0 {
-    return "row"
-  } else {
-    return "col"
-  }
+	random_number := random_number(2)
+	if random_number == 0 {
+		return "row"
+	} else {
+		return "col"
+	}
 }
 
-func searching_free_slots(direction string, len_ship int) ([]map[int]int) {
-  ship_coordinates := make([]map[int]int, 0, len_ship)
-  valid_slots := 0
-  random_slot := random_number(8)
-  ship_start_random := random_number(8 - len_ship)
+func searching_free_slots(direction string, len_ship int) []map[int]int {
+	ship_coordinates := make([]map[int]int, 0, len_ship)
+	valid_slots := 0
+	random_slot := random_number(8)
+	ship_start_random := random_number(8 - len_ship)
 
-  for valid_slots < len_ship {
-    if direction == "row" && board[ship_start_random][random_slot] == "~" {
-      coord := map[int]int{ship_start_random: random_slot}
-      ship_coordinates = append(ship_coordinates, coord)
-      ship_start_random++
-      valid_slots++
-    } else if direction == "col" && board[random_slot][ship_start_random] == "~" {
-      coord := map[int]int{random_slot: ship_start_random}
-      ship_coordinates = append(ship_coordinates, coord)
-      ship_start_random++
-      valid_slots++
-    } else {
-      ship_coordinates = make([]map[int]int, 0, len_ship)
-      random_slot = random_number(8)
-      ship_start_random = random_number(8 - len_ship)
-      valid_slots = 0
-    }
-  }
-  return ship_coordinates
+	for valid_slots < len_ship {
+		if direction == "row" && board[ship_start_random][random_slot] == "~" {
+			coord := map[int]int{ship_start_random: random_slot}
+			ship_coordinates = append(ship_coordinates, coord)
+			ship_start_random++
+			valid_slots++
+		} else if direction == "col" && board[random_slot][ship_start_random] == "~" {
+			coord := map[int]int{random_slot: ship_start_random}
+			ship_coordinates = append(ship_coordinates, coord)
+			ship_start_random++
+			valid_slots++
+		} else {
+			ship_coordinates = make([]map[int]int, 0, len_ship)
+			random_slot = random_number(8)
+			ship_start_random = random_number(8 - len_ship)
+			valid_slots = 0
+		}
+	}
+	return ship_coordinates
 }
 
 var used_ships []int
 
 func unrepeated_ship() int {
-  len_ship := random_number(4) + 1
-  for len(used_ships) < 4 {
-    used_ships = append(used_ships, len_ship)
-    for _, ship := range used_ships{
-      if len_ship != ship {
-        used_ships = append(used_ships, len_ship)
-      } else {
-        len_ship = random_number(4) + 1
-      }
-    }
-  }
-  return len_ship
+	len_ship := random_number(4) + 1
+	for len(used_ships) < 4 {
+		used_ships = append(used_ships, len_ship)
+		for _, ship := range used_ships {
+			if len_ship != ship {
+				used_ships = append(used_ships, len_ship)
+			} else {
+				len_ship = random_number(4) + 1
+			}
+		}
+	}
+	return len_ship
 }
-
 
 func placing_ships_and_dots(coordinates []map[int]int) {
-  len_ship := len(coordinates)
-  dot_coord := [][]int{{1, 1}, {1, 0}, {0, 1}, {-1, -1}, {-1, 0}, {0, -1}, {-1, 1}, {1, -1}}
-  for _, coord := range coordinates {
-    for row, col := range coord {
-      board[row][col] = strconv.Itoa(len_ship)
-      for _, value := range dot_coord {
-        if (row + value[0]) < 0 || (row + value[0]) > 7 || col + value[1] < 0 || col + value[1] > 7 {
-          continue
-        } else if board[row + value[0]][col + value[1]] != "~" {
-          continue
-        } else {
-          board[row + value[0]][col + value[1]] = "."
-        }
-      }
-    }
-  }
+	len_ship := len(coordinates)
+	dot_coord := [][]int{{1, 1}, {1, 0}, {0, 1}, {-1, -1}, {-1, 0}, {0, -1}, {-1, 1}, {1, -1}}
+	for _, coord := range coordinates {
+		for row, col := range coord {
+			board[row][col] = strconv.Itoa(len_ship)
+			for _, value := range dot_coord {
+				if (row+value[0]) < 0 || (row+value[0]) > 7 || col+value[1] < 0 || col+value[1] > 7 {
+					continue
+				} else if board[row+value[0]][col+value[1]] != "~" {
+					continue
+				} else {
+					board[row+value[0]][col+value[1]] = "."
+				}
+			}
+		}
+	}
 }
 
-
-func main(){
-  painting_sea()
-  placing_ships_and_dots(searching_free_slots(random_direction(), unrepeated_ship()))
-  fmt.Println(board)
+func main() {
+	painting_sea()
+	placing_ships_and_dots(searching_free_slots(random_direction(), unrepeated_ship()))
+	fmt.Println(board)
 }
