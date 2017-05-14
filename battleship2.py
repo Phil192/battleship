12d66cc18b6422805fd22b6351352f4794b0a359
ubik <phil192@yandex.ru>
2017-05-14 03:14:28 +0300

    _

--- battleship2.py
+++ battleship2.py
@@ -78,32 +78,3 @@ b.placing_ships_and_dots(a.searching_free_slots(a.random_direction(), b.unrepeat
 b.placing_ships_and_dots(a.searching_free_slots(a.random_direction(), b.unrepeated_ship()))
 
 for x in a.board: print(" ".join(x))
-
-
-
-'''user_board = []
-pull_of_ships = []
-
-for number in range(8):          #painting sea
-    user_board.append(["~"] * 8)
-
-while len(pull_of_ships) < 4:
-    ship_choice = input('Choose your ship!\n', )
-    if ship_choice not in pull_of_ships:
-
-        user_row = int(input('Choose ROW as number from 0 to 7 for starting point of your %s ship\n' % ship_choice, ))
-        user_col = int(input('Choose COL as number from 0 to 7 for starting point of your %s ship\n' % ship_choice, ))
-        if user_col > 7 or user_col < 0 or user_row > 7 or user_row < 0:
-            print('Input out of range')
-            continue
-        elif len(str(user_row)) != 1 or len(str(user_col)) != 1:
-            print("You need to input proper coordinate!")
-        else:
-            pull_of_ships.append(ship_choice)
-            print('Ships added: %s.\n' % ", ".join(pull_of_ships))
-    elif ship_choice in pull_of_ships:
-        print('You have chosen this one already!')
-        continue
-    elif len(str(ship_choice)) != 1:
-        print('You need to choose number of ship length. It must be integer!')
-        continue'''
